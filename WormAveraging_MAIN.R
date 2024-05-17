64fe2071834be954c44d8faa3e8353fefc32587c
Nic Vega <vega.nm@gmail.com>
2024-05-17 08:24:17 -0400

    Script for main text

--- WormAveraging_MAIN.R
+++ WormAveraging_MAIN.R
@@ -18,57 +18,83 @@ xTextSize<-14
 # FUNCTIONS
 #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-wormbootOnCounts<-function(reps, mydata, Dcorrect){
-  # Expects a number of replicates for the bootstrap (reps)
+wormbootOnCounts<-function(n_reps, mydata, batch_sizes=c(1,5,10,20,50), foldD=10, correction_constant=1){
+  # Expects a number of replicates for the bootstrap (n_reps)
   # and a data frame of worm CFU data for individuals (mydata)
   # where the number of colonies counted is in column "Count"
-  # the dilution at which these colonies were measured is in column "D"
-  # and the CFU/worm is in column "CFU"
+  # the dilution at which these colonies were measured is in column "D",
+  # FoldD(num) is fold dilution in dilution series 
+  #  (Default is 10X dilutions, e.g. each step in serial dilution is 1:10 volume)
   #
-  ## The dilution correction factor (numeric) is given as "Dcorrect"
+  ## The dilution correction factor (numeric) is given as "correction_constant"
   # and is the ratio of the original volume and the volume plated
-  # e.g. for 10 uL spots and an original volume of 1 mL, Dcorrect = 100
+  # e.g. for 10 uL spots and an original volume of 1 mL, correction_constant = 100
   # 
   # Returns a data frame of simulated batch digests
-  # with batch sizes 1, 5, 10, 20, 50 worms/batch
+  # with batch sizes in vector batch_sizes, default (1, 5, 10, 20, 50) worms/batch
   # values reported as inferred CFU/worm and log10(CFU/worm)
+  
+  # get size of data
   capp<-dim(mydata)[1]
-  batch5<-rep(0,reps)
-  batch10<-rep(0,reps)
-  batch20<-rep(0,reps)
-  batch50<-rep(0, reps)
-  for(i in 1:reps){
-    idx5<-sample(1:capp,5,replace=TRUE)
-    idx10<-sample(1:capp,10,replace=TRUE)
-    idx20<-sample(1:capp,20,replace=TRUE)
-    idx50<-sample(1:capp,50,replace=TRUE)
+  
+  # make someplace to put stuff
+  temp<-vector("list", length=length(batch_sizes)*n_reps)
+  
+  #batch5<-rep(0,n_reps)
+  #batch10<-rep(0,n_reps)
+  #batch20<-rep(0,n_reps)
+  #batch50<-rep(0, n_reps)
+  for(i in 1:n_reps){
+    #idx5<-sample(1:capp,5,replace=TRUE)
+    #idx10<-sample(1:capp,10,replace=TRUE)
+    #idx20<-sample(1:capp,20,replace=TRUE)
+    #idx50<-sample(1:capp,50,replace=TRUE)
     
-    # Assume Poisson count error
-    temp_count<-rpois(5, mydata$Count[idx5])
-    temp<-Dcorrect*temp_count*10^mydata$D[idx5]
-    batch5[i]<-mean(temp)
+    temp<-rep(0, length(batch_sizes))
+    for (j in seq_len(batch_sizes)){
+      # Randomly pull samples from data for batching
+      idx<-sample(1:capp,batch_sizes[j],replace=TRUE)
+      # Assume Poisson count error and generate new counts
+      temp_count<-rpois(batch_sizes[j], mydata$Count[idx])
+      # Calculate CFU/worm
+      temp_final<-correction_constant*temp_count*foldD^mydata$D[idx]
+      temp[j]<-mean(temp_final)
+    }
+    #temp_count<-rpois(5, mydata$Count[idx5])
+    #temp<-correction_constant*temp_count*10^mydata$D[idx5]
+    #batch5[i]<-mean(temp)
     
-    temp_count<-rpois(10, mydata$Count[idx10])
-    temp<-Dcorrect*temp_count*10^mydata$D[idx10]
-    batch10[i]<-mean(temp)
+    #temp_count<-rpois(10, mydata$Count[idx10])
+    #temp<-correction_constant*temp_count*10^mydata$D[idx10]
+    #batch10[i]<-mean(temp)
     
-    temp_count<-rpois(20, mydata$Count[idx20])
-    temp<-Dcorrect*temp_count*10^mydata$D[idx20]
-    batch20[i]<-mean(temp)
+    #temp_count<-rpois(20, mydata$Count[idx20])
+    #temp<-correction_constant*temp_count*10^mydata$D[idx20]
+    #batch20[i]<-mean(temp)
     
-    temp_count<-rpois(50, mydata$Count[idx50])
-    temp<-Dcorrect*temp_count*10^mydata$D[idx50]
-    batch50[i]<-mean(temp)
+    #temp_count<-rpois(50, mydata$Count[idx50])
+    #temp<-correction_constant*temp_count*10^mydata$D[idx50]
+    #batch50[i]<-mean(temp)
+    
+    temp[[i]]<-tibble(Batch=batch_sizes,
+                      FinalCount=temp) 
   }
-  batch5log<-log10(batch5+1)
-  batch10log<-log10(batch10+1)
-  batch20log<-log10(batch20+1) 
-  batch50log<-log10(batch50+1) 
-  Batch<-c(rep(1,times=capp), rep(5, times=reps), rep(10, times=reps), rep(20, times=reps), rep(50, times=reps))
-  mylogCFU<-log10(mydata$CFU+1)
-  logCFU<-c(mylogCFU, batch5log, batch10log, batch20log, batch50log)
-  CFU<-c(mydata$CFU, batch5, batch10, batch20, batch50)
-  dataSet<-data.frame(Batch, CFU, logCFU)
+  
+  # unfold data
+  dataSet<-dplyr::bind_rows(temp)
+  
+  dataSet<-dataSet %>%
+    mutate(logCFU=log10(FinalCount))
+  
+  #batch5log<-log10(batch5+1)
+  #batch10log<-log10(batch10+1)
+  #batch20log<-log10(batch20+1) 
+  #batch50log<-log10(batch50+1) 
+  #Batch<-c(rep(1,times=capp), rep(5, times=n_reps), rep(10, times=n_reps), rep(20, times=n_reps), rep(50, times=n_reps))
+  #mylogCFU<-log10(mydata$CFU+1)
+  #logCFU<-c(mylogCFU, batch5log, batch10log, batch20log, batch50log)
+  #CFU<-c(mydata$CFU, batch5, batch10, batch20, batch50)
+  #dataSet<-data.frame(Batch, CFU, logCFU)
   return(dataSet)
 }
 
